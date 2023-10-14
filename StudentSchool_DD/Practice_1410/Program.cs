48fe9e87c230f757826c4642918e43da77b26b66
Marat4512 <haker.320437@gmail.com>
2023-10-14 14:00:24 +0300

    add FooBar

--- StudentSchool_DD/Practice_1410/Program.cs
+++ StudentSchool_DD/Practice_1410/Program.cs
@@ -6,5 +6,16 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        static string FooBar(int num)
+        {
+            if ((num % 3 == 0) && (num % 5 == 0))
+                return "foobar";
+            if (num % 5 == 0)
+                return "bar";
+            if (num % 3 == 0)
+                return "foo";
+            return ""
+        }
     }
 }
